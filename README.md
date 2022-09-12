c687fb42b4d9ad72781589231224947deb614545
JoaoCampos89 <jcampos893@gmail.com>
2022-09-12 11:32:29 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Dexkit Default Token List
+
+Token list to be used on DexKit Open Marketplace which is composed of the most used stablecoins of all supported networks and some ERC20tokens.
+
+## Supported networks
+
+- Ethereum
+- BSC
+- Polygon
+- Avalanche
+- Fantom
+- Optimism
+- Arbitrum
+
+Code based on [Sushiswap Lists](https://github.com/sushiswap/list)
---
+++ internal/build.js
@@ -0,0 +1,44 @@
+const { version } = require("../package.json");
+
+const ethereum = require("../tokens/ethereum.json");
+const fantom = require("../tokens/fantom.json");
+const polygon = require("../tokens/polygon.json");
+const bsc = require("../tokens/bsc.json");
+const avalanche = require("../tokens/avalanche.json");
+const arbitrum = require("../tokens/arbitrum.json");
+const optimism = require("../tokens/optimism.json");
+
+function sortTokens(tokens) {
+  return tokens.sort((t1, t2) => {
+    if (t1.chainId === t2.chainId) {
+      return t1.symbol.toLowerCase() < t2.symbol.toLowerCase() ? -1 : 1;
+    }
+    return t1.chainId < t2.chainId ? -1 : 1;
+  });
+}
+
+module.exports = function buildList() {
+  const parsed = version.split(".");
+  return {
+    name: "DexKit Token List",
+    timestamp: new Date().toISOString(),
+    version: {
+      major: +parsed[0],
+      minor: +parsed[1],
+      patch: +parsed[2],
+    },
+    tags: {},
+    logoURI:
+      "https://raw.githubusercontent.com/DexKit/assets/main/images/logo_256x256.png",
+    keywords: ["dexkit", "default"],
+    tokens: sortTokens([
+      ...ethereum,
+      ...fantom,
+      ...polygon,
+      ...bsc,
+      ...avalanche,
+      ...arbitrum,
+      ...optimism,
+    ]),
+  };
+};
